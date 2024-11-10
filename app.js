5ea442043865bc6ad4dd75e6a1659f6380e18b40
Ashmeet670 <73339721+Ashmeet670@users.noreply.github.com>
2024-11-10 15:59:15 +0530

    day, date added

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
+const days = {
+    "0":"Sunday",
+    "1":"monday",
+    "2":"tuesday",
+    "3":"wednesday",
+    "4":"thursday",
+    "5":"friday",
+    "6":"saturday",
+}
+
+const months = {
+    "0": 'January',
+    "1": 'February',
+    "2": 'March',
+    "3": 'April',
+    "4": 'May',
+    "5": 'June',
+    "6": 'July',
+    "7": 'August',
+    "8": 'September',
+    "9": 'October',
+    "10": 'November',
+    "11": 'December'
+  };
+  
+
 document.getElementById("time").innerText = Date().split(" ")[4]
+document.getElementById("day").innerText = days[new Date().getDay()]
+document.getElementById("date").innerText = `${new Date().getDate()} ${months[new Date().getMonth()]},${new Date().getFullYear()}`
 
 setInterval(() => {
     document.getElementById("time").innerText = Date().split(" ")[4]
